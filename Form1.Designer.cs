945c5aa5d7a23c9dd6da2bdfc123458aef701f2e
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-12-14 10:43:35 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Average
+{
+    partial class AverageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.number1TextBox = new System.Windows.Forms.TextBox();
+            this.number2TextBox = new System.Windows.Forms.TextBox();
+            this.number3TextBox = new System.Windows.Forms.TextBox();
+            this.calculateAverageButton = new System.Windows.Forms.Button();
+            this.averagesListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(78, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Third number:";
+            // 
+            // number1TextBox
+            // 
+            this.number1TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number1TextBox.Location = new System.Drawing.Point(214, 29);
+            this.number1TextBox.Name = "number1TextBox";
+            this.number1TextBox.Size = new System.Drawing.Size(100, 29);
+            this.number1TextBox.TabIndex = 3;
+            // 
+            // number2TextBox
+            // 
+            this.number2TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number2TextBox.Location = new System.Drawing.Point(214, 64);
+            this.number2TextBox.Name = "number2TextBox";
+            this.number2TextBox.Size = new System.Drawing.Size(100, 29);
+            this.number2TextBox.TabIndex = 4;
+            // 
+            // number3TextBox
+            // 
+            this.number3TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number3TextBox.Location = new System.Drawing.Point(214, 99);
+            this.number3TextBox.Name = "number3TextBox";
+            this.number3TextBox.Size = new System.Drawing.Size(100, 29);
+            this.number3TextBox.TabIndex = 5;
+            // 
+            // calculateAverageButton
+            // 
+            this.calculateAverageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateAverageButton.Location = new System.Drawing.Point(76, 144);
+            this.calculateAverageButton.Name = "calculateAverageButton";
+            this.calculateAverageButton.Size = new System.Drawing.Size(218, 47);
+            this.calculateAverageButton.TabIndex = 6;
+            this.calculateAverageButton.Text = "Calculate Average";
+            this.calculateAverageButton.UseVisualStyleBackColor = true;
+            this.calculateAverageButton.Click += new System.EventHandler(this.calculateAverageButton_Click);
+            // 
+            // averagesListBox
+            // 
+            this.averagesListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averagesListBox.FormattingEnabled = true;
+            this.averagesListBox.ItemHeight = 24;
+            this.averagesListBox.Location = new System.Drawing.Point(76, 207);
+            this.averagesListBox.Name = "averagesListBox";
+            this.averagesListBox.Size = new System.Drawing.Size(218, 172);
+            this.averagesListBox.TabIndex = 7;
+            // 
+            // AverageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 409);
+            this.Controls.Add(this.averagesListBox);
+            this.Controls.Add(this.calculateAverageButton);
+            this.Controls.Add(this.number3TextBox);
+            this.Controls.Add(this.number2TextBox);
+            this.Controls.Add(this.number1TextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "AverageForm";
+            this.Text = "Average";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox number1TextBox;
+        private System.Windows.Forms.TextBox number2TextBox;
+        private System.Windows.Forms.TextBox number3TextBox;
+        private System.Windows.Forms.Button calculateAverageButton;
+        private System.Windows.Forms.ListBox averagesListBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Average
+ * Pawelski
+ * 12/14/2021
+ * This program calculates the average of three inputted
+ * numbers.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Average
+{
+    public partial class AverageForm : Form
+    {
+        public AverageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateAverageButton_Click(object sender, EventArgs e)
+        {
+            double num1, num2, num3, total, average;
+            num1 = Convert.ToDouble(number1TextBox.Text);
+            num2 = Convert.ToDouble(number2TextBox.Text);
+            num3 = Convert.ToDouble(number3TextBox.Text);
+            total = num1 + num2 + num3;
+            average = total / 3.0;
+            averagesListBox.Items.Add(average);
+        }
+    }
+}
